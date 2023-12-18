8baf5bd6691a7113ecde24f2460b5c234440443d
jynxio <jinxiaomatrix@gmail.com>
2023-12-19 03:10:16 +0800

    Chore

--- src/index.js
+++ src/index.js
@@ -1,34 +1,12 @@
-import inquirer from 'inquirer';
+import prompts from 'prompts';
 
-async function createProject() {
-  // 提示用户选择一个框架
-  const answers = await inquirer.prompt([
-    {
-      type: 'list',
-      name: 'framework',
-      message: 'Which framework do you want to use?',
-      choices: ['vue', 'solid', 'react'],
-    },
-  ]);
+(async () => {
+  const response = await prompts({
+    type: 'number',
+    name: 'value',
+    message: 'How old are you?',
+    validate: value => value < 18 ? `Nightclub is 18+ only` : true
+  });
 
-  // 基于用户选择处理模板
-  switch (answers.framework) {
-    case 'vue':
-      console.log('You have selected Vue. Setting up Vue template...');
-      // 这里添加处理 Vue 模板的逻辑
-      break;
-    case 'solid':
-      console.log('You have selected Solid. Setting up Solid template...');
-      // 处理 Solid 模板的逻辑
-      break;
-    case 'react':
-      console.log('You have selected React. Setting up React template...');
-      // 处理 React 模板的逻辑
-      break;
-    default:
-      console.log('Invalid choice.');
-      break;
-  }
-}
-
-createProject();
+  console.log(response); // => { value: 24 }
+})();
