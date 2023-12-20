642568e7dfc2b8d94ad8dc600e2bc950346f9d20
jynxio <jinxiaomatrix@gmail.com>
2023-12-20 22:23:54 +0800

    Feat: Improve solid.js templates

--- template-solid/vite.config.ts
+++ template-solid/vite.config.ts
@@ -2,6 +2,7 @@ import path from 'path';
 import { defineConfig } from 'vite';
 import solid from 'vite-plugin-solid';
 
+// https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [solid()],
 	resolve: {
