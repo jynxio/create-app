e97226480ec33853776fb0527bb44de95c5ba70c
jynxio <jinxiaomatrix@gmail.com>
2023-12-20 22:22:29 +0800

    Feat: Add template for React.js

---
+++ template-react/src/App.tsx
@@ -0,0 +1,14 @@
+import style from './App.module.css';
+import { useState } from 'react';
+
+function App() {
+	const [count, setCount] = useState(0);
+
+	return (
+		<button className={style.button} onClick={() => setCount(prev => prev + 1)}>
+			{count}
+		</button>
+	);
+}
+
+export default App;
