9d3baa3b0908a49a45cdcc4674c431214579e0d9
jynxio <jinxiaomatrix@gmail.com>
2024-04-06 21:26:22 +0800

    refactor: refactored the cli program

---
+++ src/color.ts
@@ -0,0 +1,11 @@
+export default {
+    vue: '#65b687',
+    react: '#499cc6',
+    solid: '#5c87c1',
+    vanilla: '#eee170',
+    white: '#fafafa',
+    black: '#09090b',
+    green: 'bef853',
+    red: '#ff0000',
+    grey: '#27272a',
+};
--- src/index.ts
+++ src/index.ts
@@ -1,82 +1,143 @@
 import fs from 'fs-extra';
-import prompts from 'prompts';
 import path from 'node:path';
+import chalk from 'chalk';
+import input from '@inquirer/input';
+import select from '@inquirer/select';
+import createRandomString from './createRandomString.js';
+import color from './color.js';
 import { cwd } from 'node:process';
 import { fileURLToPath } from 'node:url';
 
 (async function () {
-    /**
-     * 输入项目名称
-     */
-    let projectName: string;
+    console.log('\n');
+    console.log(chalk(createTag(' init ') + chalk.hex(color.grey)(' - @jynxio/create-app -')));
+    console.log();
 
-    for (;;) {
-        const response = await prompts({
-            type: 'text',
-            name: 'value',
-            message: '请输入项目名称: ',
-        });
+    const templateName = await queryTemplateName();
+    const folderName = await queryfolderName();
 
-        //
-        if (!response.value) {
-            console.log('错误: 项目名称为空');
-
-            continue;
-        }
-
-        //
-        const isExist = await fs.pathExists(path.join(cwd(), response.value));
-
-        if (isExist) {
-            console.log('错误：项目名称已被占用');
-
-            continue;
-        }
-
-        //
-        projectName = response.value;
-
-        break;
-    }
-
-    /**
-     * 选择项目模板
-     */
-    const response = await prompts({
-        type: 'select',
-        name: 'value',
-        message: '请选择项目模板: ',
-        choices: [
-            {
-                title: 'Vue',
-                value: 'vue',
-            },
-            {
-                title: 'React',
-                value: 'react',
-            },
-            {
-                title: 'Solid',
-                value: 'solid',
-            },
-        ],
-    });
-    const templateName: string = response.value;
-
-    /**
-     * 克隆项目模板
-     */
-    const projectPath = path.join(cwd(), projectName);
-    const templatePath = path.join(
-        path.dirname(path.dirname(fileURLToPath(import.meta.url))),
-        `template-${templateName}`,
-    );
+    console.log();
 
     try {
+        await cloneProject(templateName, folderName);
+
+        console.log(chalk(createTag(' done ') + chalk.hex(color.grey)(' - @jynxio/create-app -')));
+        console.log();
+        console.log(chalk(createTag(' next ') + chalk.hex(color.grey)(' Run these command to start:')));
+        console.log(chalk.hex(color.grey)(`       1. cd ./${folderName}`));
+        console.log(chalk.hex(color.grey)('       2. npm install'));
+        console.log(chalk.hex(color.grey)('       3. npm dev'));
+    } catch {
+        console.log(chalk(createTag(' bomb ') + createErrMsg(' Failed! Issues occurred during creation...')));
+    }
+
+    console.log('\n');
+
+    async function queryTemplateName() {
+        return await select({
+            message: 'Select a template:',
+            theme: {
+                prefix: createTag(' tmpl '),
+                style: {
+                    help: () => '',
+                    message: (text: string) => text,
+                    answer: (text: string) => ' ' + text.trim(),
+                    highlight: (text: string) => {
+                        const lang = text.trim() as 'vue' | 'react' | 'solid' | 'vanilla';
+                        const newText = `       * ${lang}`;
+
+                        return chalk.hex(color[lang])(newText);
+                    },
+                },
+                icon: { cursor: '' },
+            },
+            choices: [
+                {
+                    name: '       vue',
+                    value: 'vue',
+                },
+                {
+                    name: '       react',
+                    value: 'react',
+                },
+                {
+                    name: '       solid',
+                    value: 'solid',
+                },
+                {
+                    name: '       vanilla',
+                    value: 'vanilla',
+                },
+            ],
+        });
+    }
+
+    async function queryfolderName() {
+        let folderName = '';
+
+        for (;;) {
+            console.log();
+            folderName = await input({
+                message: 'Enter a file name: ',
+                default: createRandomString(),
+                theme: {
+                    prefix: createTag(' name '),
+                    style: {
+                        message: (text: string) => text,
+                        answer: (text: string) => {
+                            const folderName = text;
+
+                            // Check: is folderName empty?
+                            const isEmpty = folderName === '';
+
+                            if (isEmpty) return createErrMsg('your input is empty');
+
+                            // Check: is folderName exist?
+                            const isExist = isFolderExist(path.join(cwd(), folderName));
+
+                            if (isExist) return createErrMsg(`your input ("${folderName}") already exists`);
+
+                            // Correct!
+                            return text;
+                        },
+                        defaultAnswer: (text: string) => chalk.hex(color.grey)(text),
+                    },
+                },
+            });
+
+            if (folderName === '') continue;
+            if (isFolderExist(path.join(cwd(), folderName))) continue;
+
+            break;
+        }
+
+        return folderName;
+    }
+
+    async function cloneProject(templateName: string, folderName: string) {
+        const projectPath = path.join(cwd(), folderName);
+        const templatePath = path.join(
+            path.dirname(path.dirname(fileURLToPath(import.meta.url))),
+            `template-${templateName}`,
+        );
+
         await fs.copy(templatePath, projectPath);
-        console.log('创建成功');
-        console.log('如果你在使用git，那么请执行`npx husky init`以初始化husky（小心！.husky的内容或将会被覆写）');
-    } catch (error) {
-        console.error('创建失败：', error);
     }
 })();
+
+function isFolderExist(path: string) {
+    try {
+        const stats = fs.statSync(path);
+        return stats.isDirectory();
+    } catch {
+        return false;
+    }
+}
+
+function createTag(text: string) {
+    return chalk.hex(color.black).bgHex(color.green)(text);
+}
+
+function createErrMsg(text: string) {
+    return chalk.hex(color.red)(text);
+}
