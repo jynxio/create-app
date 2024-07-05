c5d18682025e2b2aac797f9b5209f1962519c77c
jynxio <jinxiaomatrix@gmail.com>
2024-07-05 21:39:38 +0800

    feat: imporve the vanilla.js template

--- template-vanilla/vite.config.ts
+++ template-vanilla/vite.config.ts
@@ -1,25 +1,25 @@
-import path from 'path';
-import { defineConfig } from 'vite';
+import path from "node:path";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-    base: './',
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, './src'),
-            '$': path.resolve(__dirname, './'),
-        },
-    },
-    server: {
-        host: true,
-        port: 3000,
-        open: false,
-        strictPort: true,
-        cors: true,
-    },
-    build: {
-        outDir: 'dist',
-        target: 'esnext',
-        assetsInlineLimit: 4096,
-        chunkSizeWarningLimit: 1000,
-    },
+	base: "./",
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+			$: path.resolve(__dirname, "./"),
+		},
+	},
+	server: {
+		host: true,
+		port: 3000,
+		open: false,
+		strictPort: true,
+		cors: true,
+	},
+	build: {
+		outDir: "dist",
+		target: "esnext",
+		assetsInlineLimit: 4096,
+		chunkSizeWarningLimit: 1000,
+	},
 });
